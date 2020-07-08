178477978359fe418c8c6b2e0bea1e580d9b1e3f
Maycon Gomes <maycon.gomes83@gmail.com>
2020-07-08 07:12:04 -0300

    Criação de entidades

---
+++ QuickBuy.Dominio/Entidades/ItemPedido.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickBuy.Dominio.Entidades
+{
+    public class ItemPedido
+    {
+        public int ID { get; set; }
+        public int ProdutoId { get; set; }
+        public decimal Quantidade { get; set; }
+
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,28 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int ID { get; set; }
+        public DateTime DatePedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; } 
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+        /// <summary>
+        /// Peidos podem ter 1 ou mais itens
+        /// </summary>
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
